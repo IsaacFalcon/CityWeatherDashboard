bebb5ab94234a0a96b0958ac84d16480981450fa
Isaac Falcon <ice6490@gmail.com>
2022-09-05 20:25:04 -0500

    added search button and icon, added starting makes of functions in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,16 @@
+var date = moment().format('dddd, MMMM Do YYYY');
+var time = moment().format('HH:MM:SS');
+
+
 var key = "8f86eeff2c41d4e8294b54f7d4d495b9";
-var city= "Houston";
+var city = "Houston";
+
+function weatherToday {
+  var getUrlCurrent = 
+};
+
+
+function weatherFive {
+
+}
+  
