c5ee000e419abbf8250c0d0b3b5571c972ad9159
Isaac Falcon <ice6490@gmail.com>
2022-09-12 22:18:18 -0500

    added readme info, added container to HTML, added some button styling in CSS, added multiple functions for five to eight day forecast, to save to local storage and remember city history in JS.

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
 # CityWeatherDashboard
+
+## Description
+
+-A weather dashboard that provides current and daily forecast for a city of your choosing.
+
+## Changes Made
+- Added a search bar that can be used to look up a city and display current as well as 8 day forecast.
+- Added function to save previous cities looked up and click on them.
+- Added current temp and 8 day temp
+- Added current humidity and 8 day humidity
+- Added current wind speed and 8 day wind speed
+- Added current UV with indicator of green for good or red for bad.
+
+
+## Table of Contents
+
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Usage
+
+Link:
+
+![alt Website Screenshot](./assets/images/sample.JPG)
+
+## Credits
+
+https://developer.mozilla.org/en-US/
+
+
+
+
+## License
+
+MIT License
+
+Copyright (c) [2022] [Isaac Falcon]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+---
+
+## Badges
+
+![MIT](https://img.shields.io/apm/l/vim-mode)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,77 @@
-// var date = moment().format('dddd, MMMM Do YYYY');
-// var time = moment().format('HH:MM:SS');
+var date = moment().format('dddd, MM/DD/YYYY');
+var time = moment().format('HH:MM:SS');
 
 
 var key = "8f86eeff2c41d4e8294b54f7d4d495b9";
 var city = $(".searchtext").val();
+var lsArray = JSON.parse(localStorage.getItem("searchhistory")) || [null];
 
-
+// functions to be called for clicking the search button
 $("#searchbutton").on("click", function() {
 var searchValue = $(".searchtext").val();
   console.log(searchValue);
   geoCode(searchValue);
 });
 
+// geocode function that is fetching data based on city value, also storing in local storage as an array and calling history button function
 function geoCode(cityValue) {
+  $("#fivedayforecast").empty();
+  $("#todayforecast").empty();
+  if(cityValue === null) {
+    historyButton(cityValue).addClass("gone");
+  }
+  if(lsArray.indexOf(cityValue) === -1) {
+    historyButton(cityValue);
+    lsArray.push(cityValue);
+  };
+  localStorage.setItem("searchhistory", JSON.stringify(lsArray));
+  $("#todayforecast").append(cityValue).addClass("cityname");
   fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityValue}&limit=1&appid=${key}`)
   .then(response => response.json())
   .then(data => {
     console.log(data);
     weatherToday(data[0].lat, data[0].lon);
+    weatherFive(data[0].lat, data[0].lon);
   })
 };
 
-function weatherFive() {
-//  call this in GeoCode function just like weather today, use same API call as in weather today but instead of current you will be using the daily array, have to have for loop that goes through data.daily.length
+// history button function that is adding a button for every city that you input
+function historyButton(cityValue) {
+  var cityButton = $("<button>").text(cityValue).addClass("btnstyle");
+  $("#searchhistory").append(cityButton);
+  if(cityValue === null) {
+    cityButton.addClass("gone");
+  }
 };
 
+$("#searchhistory").on("click", "button", function() {
+  geoCode($(this).text());
+});
 
+// dynamically adding cards for a daily forecast for the next days to come
+function weatherFive(lat, lon) {
+  fetch("https://api.openweathermap.org/data/3.0/onecall?lat="+lat+"&lon="+lon+"&exclude=hourly&units=imperial&appid="+key)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    for(var i = 0; data.daily.length; i++) {
+    var dailyCard = $("<div>").addClass("card5");
+    var dateCity = $("<h3>").text();
+    var dailyTemp = $("<h3>").text("Temp: "+data.daily[i].temp.day);
+    var dailyHum = $("<h3>").text("Humidity: "+data.daily[i].humidity);
+    var dailyWind = $("<h3>").text("Wind: "+data.daily[i].wind_speed);
+
+    var icon = $("<img>").attr("src", "http://openweathermap.org/img/w/"+data.daily[i].weather[0].icon+".png");
+
+    dailyCard.append(dateCity, icon, dailyTemp, dailyHum, dailyWind);
+    $("#fivedayforecast").append(dailyCard);
+    }
+    
+  })
+
+};
+
+// adding a card to provide the weather forecast for that current day, also if statement that changes uv coloring based on value
 function weatherToday(lat, lon) {
   fetch("https://api.openweathermap.org/data/3.0/onecall?lat="+lat+"&lon="+lon+"&exclude=hourly&units=imperial&appid="+key)
   .then(response => response.json())
@@ -33,22 +79,28 @@ function weatherToday(lat, lon) {
     console.log(data);
     var currentCard = $("<div>").addClass("card");
     var currentCity = $("<h2>").val();
-    var currentTemp = $("<h2>").text("temp: "+data.current.temp);
-    var currentHum = $("<h2>").text("humidity: "+data.current.humidity);
-    var currentWind = $("<h2>").text("Wind: "+data.current.wind_speed);
-    var currentUv = $("<h2>").text("UV: "+data.current.uvi)
+    var currentDate = $("<h2>").text(date);
+    var currentTemp = $("<p>").text("Temp: "+data.current.temp);
+    var currentHum = $("<p>").text("Humidity: "+data.current.humidity);
+    var currentWind = $("<p>").text("Wind: "+data.current.wind_speed);
+    var currentUv = $("<p>").text("UV: "+data.current.uvi)
       if(data.current.uvi >= 0 && data.current.uvi <= 5) {
         currentUv.attr("class", "good");
       } else {
         currentUv.attr("class", "bad");
       };
-      
-    var icon = $("<img>").attr("src", "http://openweathermap.org/img/w/"+data.current.weather[0].icon+".png");
 
-    currentCard.append(currentCity, icon, currentTemp, currentHum, currentWind, currentUv);
+
+    currentCard.append(currentCity, currentDate, currentTemp, currentHum, currentWind, currentUv);
     $("#todayforecast").append(currentCard);
 
     
   })
 };
-  
+
+
+for (var i = 0; i < lsArray.length; i++) {
+  historyButton(lsArray[i]);
+};
+
+geoCode(lsArray[lsArray.length-1]);
